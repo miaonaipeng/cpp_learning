48420d56d290dc4860b385625e41efc6c903e02a
Alex <347881849@qq.com>
2023-03-22 10:10:06 +0800

    second commit

--- basic.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by 缪乃鹏 on 3/21/23.
-//
-#include "iostream"
-#include "basic.h"
-
-void log(int x) {
-    std::cout << x << std::endl;
-}
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "basic.h"
-#include "vector"
-
-int main() {
-    std::vector<int> v = {1, 2, 3, 4, 5, 6};
-    log(v[1]);
-    return 0;
-}
